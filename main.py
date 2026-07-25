6b53d849714a099ad53e2ca58e94c131ff2afe18
VonexXT33 <josealv.neto04b@gmail.com>
2026-07-25 04:05:23 -0300

    Feature/Login

---
+++ main.py
@@ -0,0 +1,14 @@
+while True:
+
+        usuario = str(input('Usuário: '))
+        senha = str(input('Senha: '))  
+
+        if usuario == 'José Alves'and senha == 'Jp261274':
+            print('Usuário correto!!')
+            break
+        else:
+             print('Usuário não encontrado!!')
+            
+       
+        
+       
